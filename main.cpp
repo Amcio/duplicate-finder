c4a466fb3087985ba287ca0bdbeb8e89b5718124
Amcio <amcio122@gmail.com>
2023-06-22 12:31:42 +0200

    Test case and fix bug in binaryFileCompare

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@ bool binaryFileCompare(const File& file, const File& otherFile, uint32_t limit =
 class File {
     fs::directory_entry entry;
     std::size_t hash = 0;
-    struct buffer {
-        char buffer[512];
-    };
+    // struct buffer {
+    //     char buffer[512];
+    // };
     public:
         File(fs::directory_entry _entry) : entry(_entry) {}
         const fs::directory_entry getEntry() const {
@@ -60,15 +60,20 @@ class File {
 };
 
 bool binaryFileCompare(const File& file, const File& otherFile, uint32_t limit) {
-    std::ifstream fh = std::ifstream(file.getEntry().path().stem().string(), std::ios::binary | std::ios::in);
-    std::ifstream ofh = std::ifstream(otherFile.getEntry().path().stem().string(), std::ios::binary | std::ios::in);
+    std::ifstream fh = std::ifstream(file.getEntry().path().string(), std::ios::binary | std::ios::in);
+    std::ifstream ofh = std::ifstream(otherFile.getEntry().path().string(), std::ios::binary | std::ios::in);
     char fileBuffer[512], otherBuffer[512];
     uint32_t scanned = 0;
     do {
       fh.read(fileBuffer, 512);
       ofh.read(otherBuffer, 512);
-      if (memcmp(fileBuffer, otherBuffer, 512) != 0)
+      // If we are here it means the files have the same size, thus we should be able to read the same amount of data from them.
+      // Therefore it doesn't matter which gcount is used.
+      std::streamsize bytesRead = fh.gcount();
+      if (memcmp(fileBuffer, otherBuffer, bytesRead) != 0) {
         return false;
+      }
+      // Check if EOF here
       fh.seekg(512, std::ios::cur);
       ofh.seekg(512, std::ios::cur);
     } while (((fh.gcount() == ofh.gcount()) && (fh.gcount() == 512)) && (scanned < limit));
@@ -85,12 +90,17 @@ std::vector<fs::path> scanDirectory(const fs::path& directoryPath) {
 }
 
 int main(int argc, char const *argv[]) {
-    if (argc == 1) {
-      std::cout << "Usage: dupfinder DIRECTORY\n";
-      return 0;
+    // if (argc == 1 || fs::is_regular_file(fs::path(argv[1]))) {
+    //   std::cout << "Usage: dupfinder DIRECTORY\n";
+    //   return 0;
+    // }
+    // std::unordered_map<fs::path, File>();
+    // fs::path directory(argv[1]);
+    fs::path path1("test.txt"), path2("test1.txt");
+    fs::directory_entry entry1(path1), entry2(path2);
+    File file1(entry1), file2(entry2);
+    if (file1 == file2) {
+        std::cout << "files are equal\n";
     }
-    std::unordered_map<fs::path, File>();
-    fs::path directory(argv[1]);
-    
     return 0;
 }
